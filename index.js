712f3a3362f8e36effb09ee9a24225db00f38044
Cody Piazza <37965362+cpiazza01@users.noreply.github.com>
2020-07-05 19:12:28 -0400

    added some more sounds

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
 document.addEventListener("click", function(event) {
-  console.log(event.target.innerHTML);
-  if (event.target.innerHTML === "Don't Do It") {
-    danDies.play();
+  switch (event.target.innerHTML) {
+    case "Don't Do It":
+      var danDies = new Audio("sounds/dan-dies.mp3");
+      danDies.play();
+      break;
+      case "Ow":
+        var chrisOw = new Audio("sounds/chris-ow.mp3");
+        chrisOw.play();
+        break;
+        case "Oh!":
+          var timOh = new Audio("sounds/tim-oh.mp3");
+          timOh.play();
+          break;
+          case "Don Ama Ma Did":
+            var jesusDon = new Audio("sounds/don-ama-ma-did.mp3");
+            jesusDon.play();
+            break;
+    default: console.log(event.target.innerHTML);
   }
 })
-
-var danDies = new Audio("sounds/dan-dies.mp3");
-danDies.play();
