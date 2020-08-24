69e4cd0b90e5ba06dabd7373def39f928a73ae0f
Cody Piazza <37965362+cpiazza01@users.noreply.github.com>
2020-08-24 08:44:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ document.addEventListener("click", function(event) {
       var mikeFreakingOut = new Audio("sounds/mike-freaking-out.mp3");
       mikeFreakingOut.play();
       break;
-    case "Navy":
-      var navy = new Audio("sounds/mike-navy.mp3");
-      navy.play();
-      break;
-    case "Covid":
-      var mikeCovid = new Audio("sounds/mike-covid.mp3");
-      mikeCovid.play();
-      break;
+    // case "Navy":
+    //   var navy = new Audio("sounds/mike-navy.mp3");
+    //   navy.play();
+    //   break;
+    // case "Covid":
+    //   var mikeCovid = new Audio("sounds/mike-covid.mp3");
+    //   mikeCovid.play();
+    //   break;
     case "Lightning":
       var zachLightning = new Audio("sounds/zach-lightning.mp3");
       zachLightning.play();
@@ -53,18 +53,18 @@ document.addEventListener("click", function(event) {
       var timHow = new Audio("sounds/tim-how-did-this-happen.mp3");
       timHow.play();
       break;
-    case "Don Ama Ma Did":
-      var jesusDon = new Audio("sounds/don-ama-ma-did.mp3");
-      jesusDon.play();
-      break;
-    case "Ow":
-      var chrisOw = new Audio("sounds/chris-ow.mp3");
-      chrisOw.play();
-      break;
-      case "Galgamesh":
-        var galgamesh = new Audio("sounds/galgamesh.mp3");
-        galgamesh.play();
-        break;
+    // case "Don Ama Ma Did":
+    //   var jesusDon = new Audio("sounds/don-ama-ma-did.mp3");
+    //   jesusDon.play();
+    //   break;
+    // case "Ow":
+    //   var chrisOw = new Audio("sounds/chris-ow.mp3");
+    //   chrisOw.play();
+    //   break;
+    // case "Galgamesh":
+    //   var galgamesh = new Audio("sounds/galgamesh.mp3");
+    //   galgamesh.play();
+    //   break;
     default:
       console.log(event.target.innerHTML);
   }
